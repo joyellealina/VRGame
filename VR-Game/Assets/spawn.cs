48553117f4d565f8142656327497b729353a7210
Derek Fritz <derekfritz10@gmail.com>
2018-04-05 19:16:56 -0500

    Aesthetic Changes & Working UI
    Toned down the terrain levels and enlarged the scale of the town as a whole.  Also added groundworks for the UI with a button that spawns a boat in front of the player base.

---
+++ VR-Game/Assets/spawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour {
+
+	public Transform SpawnPoint;
+	public GameObject Prefab;
+
+	public void create() {
+		GameObject newObject = Instantiate (Prefab, SpawnPoint.position, SpawnPoint.rotation) as GameObject;
+		newObject.transform.localScale = new Vector3 (.2F, .2F, .2F);
+	}
+
+}
