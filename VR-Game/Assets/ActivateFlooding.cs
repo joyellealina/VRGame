92159b21332ffc984e226a2a4707dd8574791bca
Joyelle <joyellealina@gmail.com>
2018-04-06 18:15:36 -0500

    adding rain/flood button

---
+++ VR-Game/Assets/ActivateFlooding.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActivateFlooding : MonoBehaviour
+{
+    public GameObject RainPrefab;
+    public GameObject FloodWater;
+    public Button rain;
+
+    public void ToggleRain()
+    {
+        RainPrefab.SetActive(true);
+        FloodWater.SetActive(true);
+        Debug.Log("Rain is now on");
+    }
+
+    private void Update()
+    {
+        // This will trigger the slow movement of the FloodWater in the scene
+    }
+}
