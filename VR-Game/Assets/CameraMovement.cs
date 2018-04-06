5137c60ac07aa59f379163d91f9f963b26d8d6ea
Derek Fritz <derekfritz10@gmail.com>
2018-04-06 17:31:12 -0500

    VR Integration and Controller Functionality

---
+++ VR-Game/Assets/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	int speed = 2000;
+	private float rotateSpeed = 10;
+	public Vector2 leftThumbstickPosition;
+	public Vector2 rightThumbstickPosition;
+	private Rigidbody rb;
+
+	private void Awake() {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		leftThumbstickPosition = OVRInput.Get (OVRInput.Axis2D.PrimaryThumbstick);
+		rb.velocity = new Vector3 (leftThumbstickPosition.y, 0, leftThumbstickPosition.x) * speed * Time.deltaTime;
+		rightThumbstickPosition = OVRInput.Get (OVRInput.Axis2D.SecondaryThumbstick);
+		if(OVRInput.Get(OVRInput.Button.Four)){
+			rb.velocity = new Vector3 (0, 1, 0) * speed * Time.deltaTime;
+		}
+		if(OVRInput.Get(OVRInput.Button.Three)){
+			rb.velocity = new Vector3 (0, -1, 0) * speed * Time.deltaTime;
+		}
+	}
+}
