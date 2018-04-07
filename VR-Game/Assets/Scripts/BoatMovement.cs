a911dd26b40c4a9ce1dfa49b9240cbd61c2ef540
Derek Fritz <derekfritz10@gmail.com>
2018-04-07 17:39:59 -0500

    Fixed AI Tracking & Boat Spawning

---
+++ VR-Game/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoatMovement : MonoBehaviour {
+
+	[SerializeField]
+	Transform _destination;
+
+	NavMeshAgent _navMeshAgent;
+
+	public void Seek()
+	{
+		_navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+		if (_navMeshAgent == null)
+		{
+			Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
+		}
+		else
+		{
+			SetDestination();
+		}
+	}
+
+	private void SetDestination()
+	{
+		if (_destination != null)
+		{
+			Vector3 targetVector = _destination.transform.position;
+			_navMeshAgent.SetDestination(targetVector);
+		}
+	}
+}
--- VR-Game/Assets/Scripts/spawn.cs
+++ VR-Game/Assets/Scripts/spawn.cs
@@ -1,44 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class spawn : MonoBehaviour {
 
 	public Transform SpawnPoint;
 	public GameObject Prefab;
-
-    [SerializeField]
-    Transform _destination;
-
-    NavMeshAgent _navMeshAgent;
+	public GameObject Parent;
 
     public void create() {
 		GameObject newObject = Instantiate (Prefab, SpawnPoint.position, SpawnPoint.rotation) as GameObject;
 		newObject.transform.localScale = new Vector3 (.2F, .2F, .2F);
+		newObject.transform.parent = Parent.transform;
 	}
-
-    void Start()
-    {
-        _navMeshAgent = this.GetComponent<NavMeshAgent>();
-
-        if (_navMeshAgent == null)
-        {
-            Debug.LogError("The nav mesh agent component is not attached to " + gameObject.name);
-        }
-        else
-        {
-            SetDestination();
-        }
-    }
-
-    private void SetDestination()
-    {
-        if (_destination != null)
-        {
-            Vector3 targetVector = _destination.transform.position;
-            _navMeshAgent.SetDestination(targetVector);
-        }
-    }
 
 }
