03bf3b0a75ac7e72d16f19950e41fa5e9eebf0fb
Derek Fritz <derekfritz10@gmail.com>
2018-04-07 18:15:53 -0500

    Added additional boat units

--- VR-Game/Assets/Scripts/spawn.cs
+++ VR-Game/Assets/Scripts/spawn.cs
@@ -7,10 +7,11 @@ public class spawn : MonoBehaviour {
 	public Transform SpawnPoint;
 	public GameObject Prefab;
 	public GameObject Parent;
+	public float Scale;
 
     public void create() {
 		GameObject newObject = Instantiate (Prefab, SpawnPoint.position, SpawnPoint.rotation) as GameObject;
-		newObject.transform.localScale = new Vector3 (.2F, .2F, .2F);
+		newObject.transform.localScale = new Vector3 (Scale, Scale, Scale);
 		newObject.transform.parent = Parent.transform;
 	}
 
