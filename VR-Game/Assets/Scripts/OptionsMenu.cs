d69f564de5e500bfe3bb8a0c4753e82fb20aa378
Derek Fritz <derekfritz10@gmail.com>
2018-04-25 16:52:54 -0500

    Added Options Menu
    Contains sliders for changing civilian demographics but do not actually effect anything yet

---
+++ VR-Game/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour {
+
+	public TextMeshProUGUI sliderValue1;
+	public TextMeshProUGUI inverseSliderValue1;
+	public TextMeshProUGUI sliderValue2;
+	public TextMeshProUGUI inverseSliderValue2;
+	public Slider slider1;
+	public Slider slider2;
+
+	void Update () {
+		sliderValue1.text = slider1.value.ToString ("0.0");
+		inverseSliderValue1.text = (100 - slider1.value).ToString ("0.0");
+		sliderValue2.text = slider2.value.ToString ("0.0");
+		inverseSliderValue2.text = (100 - slider2.value).ToString ("0.0");
+	}
+}
