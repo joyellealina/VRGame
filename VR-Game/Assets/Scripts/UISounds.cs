29eea594aaef823248ebea322c9de1ec3557ec90
Derek Fritz <derekfritz10@gmail.com>
2018-05-07 16:52:30 -0500

    Added UI sounds and minor bug fixes

---
+++ VR-Game/Assets/Scripts/UISounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISounds : MonoBehaviour {
+
+	public AudioClip sound;
+	private Button button {get { return GetComponent<Button> (); } }
+	private AudioSource source { get { return GetComponent<AudioSource> (); } }
+
+	void Start () {
+		gameObject.AddComponent<AudioSource> ();
+		source.clip = sound;
+		source.playOnAwake = false;
+		button.onClick.AddListener (() => PlaySound ());
+	}
+	
+	void PlaySound () {
+		source.PlayOneShot (sound);
+	}
+}
