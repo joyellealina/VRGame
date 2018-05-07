85d957ae9edb44e38f95af82fdd7cccf6a65218e
Derek Fritz <derekfritz10@gmail.com>
2018-05-07 18:31:15 -0500

    Fixed some issues with the scoring system
    Also implemented for other scenarios

--- VR-Game/Assets/Scripts/ScoreMenu.cs
+++ VR-Game/Assets/Scripts/ScoreMenu.cs
@@ -9,12 +9,21 @@ public class ScoreMenu : MonoBehaviour {
 	public TextMeshProUGUI times;
 	public TextMeshProUGUI dates;
 
-	void Start () {
+	void Update () {
 		StreamReader reader = new StreamReader("Assets/Scores.txt");
 		times.text = reader.ReadToEnd ();
 		reader.Close ();
 		reader = new StreamReader("Assets/Dates.txt");
 		dates.text = reader.ReadToEnd ();
 		reader.Close ();
+	}
+
+	// Clears record of scores
+	public void Clear () {
+		StreamWriter writer = new StreamWriter ("Assets/Scores.txt");
+		writer.Write (string.Empty);
+		writer = new StreamWriter ("Assets/Dates.txt");
+		writer.Write (string.Empty);
+		writer.Close ();
 	}
 }
--- VR-Game/Assets/Scripts/WinMenu.cs
+++ VR-Game/Assets/Scripts/WinMenu.cs
@@ -10,14 +10,19 @@ public class WinMenu : MonoBehaviour {
 	public GameObject win;
 	public float gameTimer;
 	public TextMeshProUGUI winTime;
+	public string gameMode;
 
 	void Update () {
 		gameTimer += Time.deltaTime;	// Keeps track of time taken to win
 
 		if (BoatMovement.win == true) {
+			BoatMovement.win = false;	// Resets win condition
 			winTime.text = (gameTimer).ToString ("0.0"); // Display time
-			StreamWriter writer = new StreamWriter("Assets/Scores.txt");
+			StreamWriter writer = new StreamWriter("Assets/Scores.txt", append: true);
 			writer.WriteLine (winTime.text);
+			writer.Close ();
+			writer = new StreamWriter("Assets/Dates.txt", append: true);
+			writer.WriteLine (gameMode);
 			writer.Close ();
 			win.SetActive (true);
 			main.SetActive (false);
